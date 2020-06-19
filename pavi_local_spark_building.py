05e0c43efd0488973a48f218ce28a46e511352ac
gaoyuanliang <67142746+gaoyuanliang@users.noreply.github.com>
2020-06-19 20:43:58 +0400

    Create pavi_local_spark_building.py

---
+++ pavi_local_spark_building.py
@@ -0,0 +1,10 @@
+import pyspark
+from pyspark import *
+from pyspark.sql import *
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+
+spark = SparkSession.builder.master("local[2]").appName("pavi").config("spark.driver.memory", "50g").config("spark.driver.maxResultSize", "50g").getOrCreate()
+sc = spark.sparkContext
+sqlContext = SQLContext(sc)
+spark.sparkContext._conf.getAll()
